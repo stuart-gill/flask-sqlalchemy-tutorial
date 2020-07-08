45fb76329fea65538cbd8f21fa466434733a05a1
Stuart Gill <stuart.a.gill@gmail.com>
2020-07-08 16:51:36 -0700

    add sqlalchemy, have item and user models connected but not actually using sqlalchemy yet

--- section6/code/app.py
+++ section6/code/app.py
@@ -5,8 +5,11 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from db import db
 
 app = Flask(__name__)
+# confusing-- I think this keep flask from tracking changes but lets SQLalchemy do it??
+app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
 app.secret_key = "stuart"
 api = Api(app)
 
@@ -22,4 +25,5 @@ api.add_resource(UserRegister, "/register")
 # conditional ensures that app is only run when we run app.py, and not if/when we import it to another file
 # only the file you directly run is __main__
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
--- section6/code/models/user.py
+++ section6/code/models/user.py
@@ -1,10 +1,19 @@
 import sqlite3
+from db import db
 
 # user is in models instead of resources because there are no API endpoints for User information
 # model is the internal representation, resource is the external representation
 
 
-class UserModel:
+class UserModel(db.Model):
+
+    # these next lines are how you connect to database
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
